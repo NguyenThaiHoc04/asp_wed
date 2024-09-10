f698c7121bfb6846e568913d79f40ac81ac18d91
nguyen thai hoc <nguyenthaihoc171104@gmail.com>
2024-09-10 15:13:40 +0700

    add BaiKiemTra01

---
+++ BaiKiemTra01/BaiKiemTra01/Controllers/DangKyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra01.Controllers
+{
+    public class DangKyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BaiKiemTra01/BaiKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BaiKiemTra01.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public string TaiKhoan { get; set; }
+        public string MatKhau { get; set; }
+        public string Ten { get; set; }
+        public int  Tuoi { get; set; }
+    }
+}
